5a92209e88f6b66c214e008337f7997952c3f11b
Yanka28 <125182038+Yanka28@users.noreply.github.com>
2023-06-28 20:50:43 +0000

    Deploying to gh-pages from @ Yanka28/goit-js-hw-08@bc006dc6f361a97bf402563d96d54d56add23f02 🚀

---
+++ index.b5baf513.js
@@ -0,0 +1,2 @@
+console.log(iiiii),console.log(iiiii);
+//# sourceMappingURL=index.b5baf513.js.map
---
+++ index.b7400216.js
@@ -0,0 +1,2 @@
+console.log(iiiii),console.log(iiiii);
+//# sourceMappingURL=index.b7400216.js.map
