e583b71c90cb93aa6ffb8c32a76fe103edb1c3ba
Yanka28 <yanachepeluk@ukr.net>
2023-07-04 22:34:58 +0300

    commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,32 @@
 
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframeEl = document.querySelector('#vimeo-player')
+const player = new Player(iframeEl);
+const LS_SET = "videoplayer-current-time"
+
+const onPlay = function ({ seconds }) {
+    localStorage.setItem(LS_SET, JSON.stringify(seconds))
+   
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const LS_GET = localStorage.getItem(LS_SET)
+ console.log(LS_GET);
+player.setCurrentTime(Number(LS_GET)).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
+});
+
+
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('form')
+const inputEl = document.querySelector('input')
+const textareaEl = document.querySelector('textarea')
+console.dir(inputEl);
+console.dir(textareaEl)
+console.dir(formEl);
+
+formEl.addEventListener('input', throttle(onInput,500))
+formEl.addEventListener('submit', onSubmit)
+window.addEventListener('load', (event) => { 
+    if (FFS_GET) { fillForm() }
+})
+   
+const FFS = 'feedback-form-state'
+
+function onInput(event) {
+    const formObj = Object.fromEntries(new FormData(event.currentTarget))
+    localStorage.setItem(FFS, JSON.stringify(formObj)) 
+   
+}
+ 
+function onSubmit(event) { 
+    event.preventDefault()
+    const { email, message } = event.currentTarget.elements
+    console.log({ email: `${email.value}`, message: `${message.value}` });
+    event.currentTarget.reset()
+    localStorage.clear();
+
+}
+
+const FFS_GET = localStorage.getItem(FFS)
+console.log(FFS_GET);
+
+function fillForm() {
+
+    try {
+        inputEl.value = JSON.parse(FFS_GET).email
+        textareaEl.value = JSON.parse(FFS_GET).message
+        console.log(inputEl.value);
+        console.log(textareaEl.value);
+    }
+      
+    catch (error) {
+        console.log(error);
+    }
+}
+   
+
+
+    
